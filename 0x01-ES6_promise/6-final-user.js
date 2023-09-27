0b7863ad1d105d6a779f036e91ddd00efe44fb1b
Michael-Gichamu <michaelmatere606@gmail.com>
2023-09-28 00:43:44 +0300

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  if (typeof firstName === 'string' && typeof lastName === 'string' && typeof fileName === 'string') {
+    const userPromise = signUpUser(firstName, lastName);
+    const photoPromise = uploadPhoto(fileName);
+
+    return Promise.allSettled([userPromise, photoPromise])
+      .then((results) => results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      })));
+  }
+
+  return Promise.reject(new Error('All parameters must be strings.'));
+}
