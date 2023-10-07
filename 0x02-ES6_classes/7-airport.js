d8ded6b9bd244727fdaa6e946d0ed517d12aa05b
Michael-Gichamu <michaelmatere606@gmail.com>
2023-10-07 11:11:28 +0300

    Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,20 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name === 'string' && typeof code === 'string') {
+      this._name = name;
+      this._code = code;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
