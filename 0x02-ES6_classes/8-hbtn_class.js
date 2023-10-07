20f1db5db8f9fdeca7c0f5939318944d489cc673
Michael-Gichamu <michaelmatere606@gmail.com>
2023-10-07 11:12:03 +0300

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,26 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size === 'number' && typeof location === 'string') {
+      this._size = size;
+      this._location = location;
+    }
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
